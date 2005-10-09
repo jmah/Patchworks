8547f174ae07d238228336157f9382245de3719d
Jonathon Mah <jonathon@playhaus.org>
2005-10-09 10:30:48 -0700

    Added PWPreferencesController and implemented MainMenu.nib
    For some crazy reason I can't set both the minimum and maximum height of the
    preferences window to the same value (so that it will only resize
    horizontally). I'm sure I could do this in earlier versions of Interface
    Builder. Maybe I'll have to do it in code.
    
    darcs-hash:20051009173048-5716c-354dafd2a3b977aaf7cac171798f30b78ea9dbc0.gz

---
+++ Application Source/Classes/Controller/PWPreferencesController.m
@@ -0,0 +1,93 @@
+//
+//  PWPreferencesController.m
+//  Patchworks
+//
+//  Created by Jonathon Mah on 2005-10-09.
+//  Copyright Playhaus 2005. All rights reserved.
+//  License information is contained at the bottom of this file and in the
+//  'LICENSE.txt' file.
+//
+
+#import "PWPreferencesController.h"
+#import "PatchworksDefines.h"
+
+
+@implementation PWPreferencesController
+
+#pragma mark UI Actions
+
+- (IBAction)chooseFullPatchFont:(id)sender
+{
+	NSFontDescriptor *fullPatchFontDescriptor = [NSFontDescriptor fontDescriptorWithFontAttributes:[[NSUserDefaults standardUserDefaults] objectForKey:PWFullPatchFontDescriptorAttributes]];
+	NSFontManager *fontManager = [NSFontManager sharedFontManager];
+	[fontManager setSelectedFont:[NSFont fontWithDescriptor:fullPatchFontDescriptor size:[fullPatchFontDescriptor pointSize]] isMultiple:NO];
+	[fontManager orderFrontFontPanel:self];
+}
+
+
+
+#pragma mark NSFontManager Delegate Methods
+
+- (void)changeFont:(id)sender
+{
+	NSFontDescriptor *oldFontDescriptor = [NSFontDescriptor fontDescriptorWithFontAttributes:[[NSUserDefaults standardUserDefaults] objectForKey:PWFullPatchFontDescriptorAttributes]];
+	NSFont *oldFont = [NSFont fontWithDescriptor:oldFontDescriptor size:[oldFontDescriptor pointSize]];
+	NSFont *newFont = [sender convertFont:oldFont];
+	NSFontDescriptor *newFontDescriptor = [newFont fontDescriptor];
+	
+	[self willChangeValueForKey:@"fullPatchFontString"];
+	[[NSUserDefaults standardUserDefaults] setObject:[newFontDescriptor fontAttributes] forKey:PWFullPatchFontDescriptorAttributes];
+	[self didChangeValueForKey:@"fullPatchFontString"];
+}
+
+
+
+#pragma mark Accessor Methods
+
+- (NSString *)fullPatchFontString
+{
+	NSFontDescriptor *fullPatchFontDescriptor = [NSFontDescriptor fontDescriptorWithFontAttributes:[[NSUserDefaults standardUserDefaults] objectForKey:PWFullPatchFontDescriptorAttributes]];
+	float pointSize = [fullPatchFontDescriptor pointSize];
+	
+	if (pointSize == 0.f)
+#warning I don't like having to hard-code this value
+		pointSize = 12.f;
+	
+	return [NSString stringWithFormat:@"%@ - %.1f", [[fullPatchFontDescriptor fontAttributes] objectForKey:NSFontNameAttribute], pointSize];
+}
+
+
+@end
+
+
+
+/*
+ * Patchworks is licensed under the BSD license, as follows:
+ * 
+ * Copyright (c) 2005, Playhaus
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of the Playhaus nor the names of its contributors may be
+ *   used to endorse or promote products derived from this software without
+ *   specific prior written permission.
+ * 
+ * This software is provided by the copyright holders and contributors "as is"
+ * and any express or implied warranties, including, but not limited to, the
+ * implied warranties of merchantability and fitness for a particular purpose
+ * are disclaimed. In no event shall the copyright owner or contributors be
+ * liable for any direct, indirect, incidental, special, exemplary, or
+ * consequential damages (including, but not limited to, procurement of
+ * substitute goods or services; loss of use, data, or profits; or business
+ * interruption) however caused and on any theory of liability, whether in
+ * contract, strict liability, or tort (including negligence or otherwise)
+ * arising in any way out of the use of this software, even if advised of the
+ * possibility of such damage.
+ */
